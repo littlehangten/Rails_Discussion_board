db1750ad6438919055a84f9ba0db67217dbe783c
Daniel <littlehangten@gmail.com>
2019-07-13 15:53:29 +0800

    implement groups#destory

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,6 +30,13 @@ class GroupsController < ApplicationController
     redirect_to groups_path, notice: "更新成功"
   end
 
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "刪除囉！"
+    redirect_to groups_path
+  end
+
   private
 
   def group_params
